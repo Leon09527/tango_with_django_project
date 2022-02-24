2f8e35580cb3b04ea6c1bbabd09ec283194be216
windows10 <windows10@test.com>
2022-02-24 18:36:08 +0800

    chapter 9 finish

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -27,8 +28,18 @@ class Page(models.Model):
 
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=TITLE_MAX_LENGTH)
-    url = models.URLField()
+    url = models.URLField(null=True)
     views = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
+
